e5f8031770d9ff1e6a1963d59d56214adecc5296
Cole Sammons <colemsammons@gmail.com>
2021-07-28 13:48:09 -0700

    refactoring and helper handlebars functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 require('dotenv').config();
 
 const app = express();
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({helpers});
 
 const sess = {
   secret: process.env.DB_SECRET,
